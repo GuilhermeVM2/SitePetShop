bbbf179b690600636e3b49e1db8f023a330879d4
vitorssauro <vitorvtr2003@gmail.com>
2023-12-11 00:13:37 -0300

    10/12/2023

--- TesteGPT/cadastro.php
+++ TesteGPT/cadastro.php
@@ -6,10 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <!-- Adicione o link para o CSS do Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="style/stylecadastro.css" rel="stylesheet">
 </head>
+<script>
+    function validarSenha() {
+        var senha = document.getElementById("senha").value;
+        var confirmarSenha = document.getElementById("confirmar-senha").value;
+
+        if (senha !== confirmarSenha) {
+            alert("As senhas não coincidem. Por favor, verifique.");
+            return false;
+        }
+
+        return true;
+    }
+</script>
 
 <body>
     <!-- Cabeçalho -->
@@ -17,10 +29,8 @@
         <div class="">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.html"><img src="img/logo.jpg" alt="Logo do Meu Site"
-                            class="img-fluid" style="height: 50PX;width: 50px;"></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <a class="navbar-brand" href="index.html"><img src="img/logo.jpg" alt="Logo do Meu Site" class="img-fluid" style="height: 50PX;width: 50px;"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
@@ -59,7 +69,8 @@
             }
             ?>
 
-            <form action="processa_formulario.php" method="post">
+            <form action="processa_formulario.php" method="post" onsubmit="return validarSenha();">
+
                 <label for="nome">Nome:</label>
                 <input type="text" id="nome" name="nome" required>
 
@@ -147,9 +158,7 @@
 
 </body>
 <!-- Adicione o link para o JavaScript do Bootstrap (opcional, mas recomendado para algumas funcionalidades) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 
 </html>
